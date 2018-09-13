65d33ab33b7f0db757962ce0330d0be5ef358c35
Rolf Zambon <led479.rs@gmail.com>
2018-09-13 14:47:52 -0300

    Adicionado testes e validates para password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -60,4 +60,14 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_equal email_bugado.downcase, @user.reload.email
   end
+
+  test "password should be present" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "aaaaa"
+    assert_not @user.valid?
+  end
 end
