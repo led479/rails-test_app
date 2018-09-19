b6eaec37a8729bf1df206f02eb6074f2d13fc812
Rolf Zambon <led479.rs@gmail.com>
2018-09-19 17:57:55 -0300

    Adicionado atributo admin pro user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+    @user2 = users(:jonas)
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
@@ -11,4 +17,15 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@user2)
+    assert_not @user2.admin?
+    patch user_path(@user2), params: {
+                              user: {
+                                  password: "password",
+                                  password_confirmation: "password",
+                                  admin: true
+                              }}
+    assert_not @user2.reload.admin?
+  end
 end
