5388115fc6a2fc5c9b09baa62ae736f420623834
Rolf Zambon <led479.rs@gmail.com>
2018-09-20 16:11:18 -0300

    Agora usuários que logam persistem após fechar o navegador

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,8 +9,14 @@ module SessionsHelper
   end
 
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
@@ -19,6 +25,7 @@ module SessionsHelper
   end
 
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
@@ -32,5 +39,17 @@ module SessionsHelper
   # Stores the URL trying to be accessed
   def store_location
     session[:forwarding_url] = request.original_url if request.get?
+  end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 end
