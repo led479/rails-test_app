0035954d71a62d339cfeaf428086d0c7fe4a0edb
Rolf Zambon <led479.rs@gmail.com>
2018-09-20 17:24:05 -0300

    Terimado a implementação do advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -17,6 +17,10 @@ class ActiveSupport::TestCase
     post login_path, params: { session: { email: user.email, password: 'password'}}
   end
 
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email, password: password, remember_me: remember_me}}
+  end
+
   def log_out_test
     delete logout_path
   end
