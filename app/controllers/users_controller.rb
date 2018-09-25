e711bf6a35cbab0306fe72e94291d5bb93e187ec
Rolf Zambon <led479.rs@gmail.com>
2018-09-25 17:44:51 -0300

    Terminado os testes preliminares de microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,13 +51,6 @@ class UsersController < ApplicationController
 
   private
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
 
     def correct_user
       @user = User.find(params[:id])
