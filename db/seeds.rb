17e70aa0fca4b301143dd562913e87e39b72bd47
Rolf Zambon <led479.rs@gmail.com>
2018-09-27 17:09:15 -0300

    Mudado as seeds para lotr

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ User.create!(name: "Example User",
              activated_at: Time.zone.now)
 
 99.times do |n|
-  name = Faker::Name.name
+  name = Faker::LordOfTheRings.character
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name: name, email: email,
